f4402822399d8dc8620b8aeaf6d50d5b7bc00308
mpizarro <maximilianopi@buenosaires.gob.ar>
2020-02-10 10:19:53 -0300

    change endpoint service

--- src/main/java/com/test/TestReporte.java
+++ src/main/java/com/test/TestReporte.java
@@ -15,13 +15,13 @@ public class TestReporte {
 
 		HostABM se = sist.getHostABM();
 		
-		SendEmail sendemail=sist.getSendEmail();
+		// SendEmail sendemail=sist.getSendEmail();
 		
-		try {
-			sendemail.enviarEmail("maximiliano.pizarro.5@gmail.com", se.traerHost("80-FA-5B-2C-72-49"), se.traerHost("80-FA-5B-2C-72-49"));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		// try {
+		// 	sendemail.enviarEmail("maximiliano.pizarro.5@gmail.com", se.traerHost("80-FA-5B-2C-72-49"), se.traerHost("80-FA-5B-2C-72-49"));
+		// } catch (Exception e) {
+		// 	e.printStackTrace();
+		// }
 		
 
 	}
